0c9cbfca25e6ced4becc7cac7dada35b34ad3ba3
PlayerXYT <playerxyt@yahoo.com>
2022-07-14 10:50:12 -0400

    Added comments

--- src/com/x/noreport/ColorManager.java
+++ src/com/x/noreport/ColorManager.java
@@ -13,19 +13,50 @@ import com.earth2me.essentials.Essentials;
 import net.luckperms.api.LuckPerms;
 
 public final class ColorManager {
+	// 8 bits of active plugins to save memory
 	private static byte activePlugins = 0;
 	
+	// Constants for setting and getting plugins
+	// Constant = 2^pluginPosition
 	public static final byte ESSX = 1;
 	public static final byte LP = 2;
 	
+	// Luckperms plugin integration
+	private static LuckPerms lp;
+	
+	/*
+	 * Initializes the color manager
+	 * Should be called only once
+	 */
+	public static void init() {
+		try {
+			lp = Bukkit.getServicesManager().getRegistration(LuckPerms.class).getProvider();
+		} catch(Exception e) {}
+	}
+	
+	/*
+	 * Sets the active plugin with bit math mask
+	 * @param plugin The plugin to set, use above constants
+	 * @param active What the bit should be set as
+	 */
 	public static void setActive(byte plugin, boolean active) {
 		activePlugins = (byte) (activePlugins & ~plugin);
 		activePlugins += plugin * (active?1:0);
 	}
+	/*
+	 * Gets if the plugin is registered as active with bit math mask
+	 * @param plugin The plugin to get, use above constants
+	 * @return If the plugin is active
+	 */
 	public static boolean getActive(byte plugin) {return (activePlugins & plugin) > 1;}
 	
-	public static final LuckPerms lp = Bukkit.getServicesManager().getRegistration(LuckPerms.class).getProvider();
-	
+	/*
+	 * Mimic the formatting of any active plugin or use the Minecraft default
+	 * <Player> Message
+	 * @param player The message sender
+	 * @param message The message sent
+	 * @return The formatted text
+	 */
 	public static String getColoredText(Player player, String message) {
 		
 		ChatColor color = ChatColor.RESET;
@@ -59,9 +90,21 @@ public final class ColorManager {
 		return format(color.toString(), player, message);
 	}
 	
+	/*
+	 * String.format() with less parameters
+	 * @param color The username's color
+	 * @param player The message sender
+	 * @param message The message
+	 * @return The concatonated message
+	 */
 	public static String format(String color, Player player, String message) {
 		return String.format("<%s%s%s> %s", color, player.getDisplayName(), ChatColor.RESET, message);
 	}
 	
+	/*
+	 * Short form for testing if a plugin is active
+	 * @param name The name of the plugin (see plugin.yml)
+	 * @return If the plugin is active on this server
+	 */
 	public static boolean getPlugin(String name) {return Bukkit.getPluginManager().getPlugin(name)!=null;}
 }
--- src/com/x/noreport/Main.java
+++ src/com/x/noreport/Main.java
@@ -11,18 +11,26 @@ public class Main extends JavaPlugin implements Listener {
 	
 	@Override
 	public void onEnable() {
+		// Initialize the color manager
+		ColorManager.init();
+		
+		// Register chat events
 		getServer().getPluginManager().registerEvents(this, this);
 		
+		// Set soft depended plugins for plugin integration
 		if(ColorManager.getPlugin("Essentials")) ColorManager.setActive(ColorManager.ESSX, true);
 		if(ColorManager.getPlugin("Luckperms")) ColorManager.setActive(ColorManager.LP, true);
 	}
 	
+	// All chat events will be cancelled to prevent message association with the player
 	@EventHandler
 	public void onChat(PlayerChatEvent e) {
 		e.setCancelled(true);
+		// Format text based on active plugins
 		String message = ColorManager.getColoredText(e.getPlayer(), e.getMessage());
+		// Send message from the server to prevent any reports
 		for(Player p : getServer().getOnlinePlayers()) {
-			p.sendMessage(ColorManager.getColoredText(e.getPlayer(), message));
+			p.sendMessage(message);
 		}
 	}
 	
